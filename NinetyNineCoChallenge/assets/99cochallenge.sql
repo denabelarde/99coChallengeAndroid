ea91351a69dfe37a585ba51b543275a4de0b8f1d
Denver Abelarde <denver@mobilemo.com>
2014-07-08 19:22:44 +0800

    Changelogs: 1. Added DatabaseHelper 2. Added 99cochallenge.sql file

---
+++ NinetyNineCoChallenge/assets/99cochallenge.sql
@@ -0,0 +1,9 @@
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `favoritestores` (
+  `_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `place_id` text NOT NULL,
+  `name` text NOT NULL
+);
